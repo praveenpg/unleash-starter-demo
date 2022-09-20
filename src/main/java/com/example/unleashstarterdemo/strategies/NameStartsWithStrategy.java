030fe8e7398e4fd7825b44151b462a5c939d8244
praveen <praveen.govindan@gmail.com>
2022-09-19 21:15:24 -0700

    Fixed bug in NameStartsWithStrategy

--- src/main/java/com/example/unleashstarterdemo/strategies/NameStartsWithStrategy.java
+++ src/main/java/com/example/unleashstarterdemo/strategies/NameStartsWithStrategy.java
@@ -5,13 +5,14 @@ import io.getunleash.strategy.Strategy;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Map;
 
 @Component
 public class NameStartsWithStrategy implements Strategy {
     @Override
     public String getName() {
-        return "NameStartsWithStrategy";
+        return "name-starts-with";
     }
 
     @Override
@@ -21,10 +22,10 @@ public class NameStartsWithStrategy implements Strategy {
 
     @Override
     public boolean isEnabled(Map<String, String> parameters, UnleashContext unleashContext) {
-        final var paramValue = parameters.get("paramName");
+        final var paramValue = parameters.get("paramName").split(",");
         final String name = unleashContext.getProperties().get("name");
 
-        return StringUtils.startsWith(name, paramValue);
+        return Arrays.stream(paramValue).anyMatch(a -> StringUtils.startsWith(name.toLowerCase(), a.toLowerCase()));
     }
 
     public static UnleashContext buildContext(String value) {
